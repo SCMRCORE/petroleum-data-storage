c298a2a148be6ddc6fbce875b92bb1082e94885e
SCMRCORE <3551147139@qq.com>
2024-09-17 14:30:06 +0800

    大大小小咋杂七杂八的bug

--- petroleum-server/src/main/java/com/petroleumserver/PetroleumServerApplication.java
+++ petroleum-server/src/main/java/com/petroleumserver/PetroleumServerApplication.java
@@ -1,6 +1,7 @@
 package com.petroleumserver;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableTransactionManagement // 开启注解方式的事务管理
-@Slf4j
+@MapperScan("com.petroleumserver.mapper")
 public class PetroleumServerApplication {
 
     public static void main(String[] args) {
