831d040d43ce17193ed5003438e1e931a68e4650
SCMRCORE <3551147139@qq.com>
2024-11-09 20:52:36 +0800

    修复index.tsx的旧组件调用

--- web/src/pages/DataManagement/components/WordPreviewer/index.tsx
+++ web/src/pages/DataManagement/components/WordPreviewer/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { renderAsync } from "docx-preview";
+// import { renderAsync } from "docx-preview";
 
 interface WordPreviewerProps {
   fileUrl: string;
@@ -8,30 +8,30 @@ interface WordPreviewerProps {
 
 const WordPreviewer: React.FC<WordPreviewerProps> = ({ fileUrl, onError }) => {
   const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const loadAndRenderDoc = async () => {
-      try {
-        // 获取文档内容
-        const response = await fetch(fileUrl);
-        const blob = await response.blob();
-
-        // 确保容器存在
-        if (containerRef.current) {
-          // 渲染文档
-          await renderAsync(blob, containerRef.current, containerRef.current, {
-            className: "docx-viewer",
-            // inWrapper: true,
-          });
-        }
-      } catch (error) {
-        console.error("文档预览失败:", error);
-        onError?.(error as Error);
-      }
-    };
-
-    loadAndRenderDoc();
-  }, [fileUrl, onError]);
+  //
+  // useEffect(() => {
+  //   const loadAndRenderDoc = async () => {
+  //     try {
+  //       // 获取文档内容
+  //       const response = await fetch(fileUrl);
+  //       const blob = await response.blob();
+  //
+  //       // 确保容器存在
+  //       if (containerRef.current) {
+  //         // 渲染文档
+  //         await renderAsync(blob, containerRef.current, containerRef.current, {
+  //           className: "docx-viewer",
+  //           // inWrapper: true,
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.error("文档预览失败:", error);
+  //       onError?.(error as Error);
+  //     }
+  //   };
+  //
+  //   loadAndRenderDoc();
+  // }, [fileUrl, onError]);
   return (
     // <div style={{ width: "100%", height: "70vh" }}>
     <iframe
