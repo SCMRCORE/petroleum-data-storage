929c4a1e0f160ceb9009e485f3cec726ba222fe5
erasernoob <1490910338@qq.com>
2025-04-25 19:18:10 +0800

    后端spring初始化wits0客户端整合

---
+++ web/src/pages/ChartsTest/index.tsx
@@ -0,0 +1,11 @@
+import LineChartComponent from '../../layouts/BasicLayout/components/Charts/index.tsx';
+
+const ChartsTest = () => {
+    return (
+        <div>
+            <h1>折线图展示</h1>
+            <LineChartComponent />
+        </div>
+    );
+};
+export default ChartsTest;
